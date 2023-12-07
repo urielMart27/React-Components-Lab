d16c27d3c33e37a8fbe8d47b2d93b6532797c42e
Uriel Martinez <martinezuriel651@gmail.com>
2023-12-07 16:23:03 -0600

    Render Name in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import NameBadge from "./components/NameBadge/NameBadge";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NameBadge />
+    </div>
+  );
 }
 
 export default App;
